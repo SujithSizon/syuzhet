15f63f7fd55790a99070fe65b6497df74685dfd6
VP Nagraj <vpnagraj@virginia.edu>
2015-04-16 15:24:31 -0400

    added a comparison set of tweets and more comments and updated tweet plot

--- syuzhetDemo.R
+++ syuzhetDemo.R
@@ -1,26 +1,35 @@
 ## sentiment analysis with syuzhet
 ## vp nagraj
-## last modified 3.11.15
-## adapted from syuzhet vignette: https://github.com/mjockers/syuzhet/blob/master/inst/doc/syuzhet-vignette.R
+## last modified 4.16.15
 
 setwd("~/Sites/Shiny/syuzhet/")
 
 ## install and load package — available via devtools
 ## install.packages("devtools")
 
-devtools::install_github("mjockers/syuzhet")
+## devtools::install_github("mjockers/syuzhet")
 library(syuzhet)
 
 ## take a look at the functions
 ls("package:syuzhet")
 
-## read the vignette
-vignette("syuzhet-vignette")
+## compare with other text analysis packages
 
-## PART I — Visualizing Narrative Structure
+## install.packages("tm")
+library(tm)
+ls("package:tm")
+
+## install.packages("qdap")
+library(qdap)
+ls("package:qdap")
+
+## PART I — Narrative Structure (Moby Dick)
+
+## partially adapted from syuzhet vignette: https://github.com/mjockers/syuzhet/blob/master/inst/doc/syuzhet-vignette.R
+## vignette("syuzhet-vignette")
 
 ## load data
-path_to_text <- "http://www.gutenberg.org/cache/epub/2701/pg2701.txt"           
+path_to_text <- "http://www.gutenberg.org/cache/epub/2701/pg2701.txt"
 
 ## note: only works with http
 
@@ -30,6 +39,7 @@ moby_d <- get_text_as_string(path_to_text)
 moby_d <- get_sentences(moby_d)
 
 ## get sentiment vector — bing method
+
 moby_d_sent_bing <- get_sentiment(moby_d, method="bing")
 
 moby_d_sent_bing
@@ -93,9 +103,14 @@ head(moby_d_sent_nrc2)
 moby_d_anticipation <- which(moby_d_sent_nrc2$anticipation > 0)
 moby_d[moby_d_anticipation]
 
+## clean-up work space 
+
+rm(list= ls())
+
 ## PART II — Sentiment Analysis (Twitter Data)
 
 load("~/Sites/Shiny/tweets.Rda")
+load("~/Sites/Shiny/tweets2.Rda")
 library(twitteR)
 
 ## tweets come in as a list object — maybe there's a twitteR function to handle that
@@ -105,61 +120,125 @@ ls("package:twitteR")
 ## ... and there is — twListToDF()
 
 obama_tweets_df <- twListToDF(obama_tweets)
+obama_tweets_df2 <- twListToDF(obama_tweets2)
 
-## also need to clean up invalid characters
+## get rid of the list of tweets to speed things up
+
+rm(obama_tweets,obama_tweets2)
+
+## also need to clean up invalid characters with regex ... don't ask
 
 obama_tweets_df$text <- gsub("[^0-9A-Za-z///' ]", " ", obama_tweets_df$text)
+obama_tweets_df2$text <- gsub("[^0-9A-Za-z///' ]", " ", obama_tweets_df2$text)
 
 ## now apply the get_nrc_sentiment function to the text variable
 
 obama_tweets_nrc <- get_nrc_sentiment(obama_tweets_df$text)
+obama_tweets_nrc2 <- get_nrc_sentiment(obama_tweets_df2$text)
 
 summary(obama_tweets_nrc)
+summary(obama_tweets_nrc2)
 
 library(tidyr)
 library(dplyr)
 
 tidy_obama_tweets <- 
         obama_tweets_nrc %>%
-#         select(1:8) %>%
+        select(1:8) %>%
+        gather() %>%
+        group_by(Sentiment=key) %>%
+        summarise(Total=sum(value))
+
+tidy_obama_tweets2 <- 
+        obama_tweets_nrc2 %>%
+        select(1:8) %>%
         gather() %>%
         group_by(Sentiment=key) %>%
         summarise(Total=sum(value))
 
 summary(tidy_obama_tweets)
+summary(tidy_obama_tweets2)
 
-p_obama <- ggplot(tidy_obama_tweets, aes(Sentiment,Total)) +
-        geom_bar(stat="identity") +
+library(ggplot2)
+
+# p_obama <- ggplot(tidy_obama_tweets, aes(Sentiment,Total)) +
+#         geom_bar(stat="identity") +
+#         ylab("Total Weight") +
+#         ggtitle("Obama Tweet Sentiment (3/11/15)")
+# 
+# p_obama
+
+p_obama <- ggplot(NULL, aes(Sentiment,Total)) +
+        geom_bar(aes(fill="march"), data=tidy_obama_tweets, stat="identity", alpha=0.5) +
+        geom_bar(aes(fill="april"), data=tidy_obama_tweets2, stat="identity", alpha=0.5) +
         ylab("Total Weight") +
-        ggtitle("Obama Tweet Sentiment (3/11/15)")
+        theme(legend.title=element_blank()) +
+        ggtitle("Obama Tweet Sentiment (March & April)")
 
 p_obama
 
-## get sentiment with bing
+## now let's look at positive vs. negative
 
-obama_sent_bing <- get_sentiment(obama_tweets_df$text, method="bing")
+tidy_obama_tweets_polarity <- 
+        obama_tweets_nrc %>%
+        select(9:10) %>%
+        gather() %>%
+        group_by(Sentiment=key) %>%
+        summarise(Total=sum(value))
 
-plot(
-        obama_tweets_df$created,
-        obama_sent_bing,
-        type="l", 
-        main="Sentiment Over Time", 
-        xlab = "Time", 
-        ylab= "Emotional Valence",
-        col="red"
-)
+tidy_obama_tweets_polarity2 <- 
+        obama_tweets_nrc2 %>%
+        select(9:10) %>%
+        gather() %>%
+        group_by(Sentiment=key) %>%
+        summarise(Total=sum(value))
 
-obama_sent_bing_ft <- get_transformed_values(obama_sent_bing)
+summary(tidy_obama_tweets_polarity)
+summary(tidy_obama_tweets_polarity2)
 
-plot(
-        obama_sent_bing_ft, 
-        type="l", 
-        main="Sentiment Over Time", 
-        xlab = "Time (Percentage)", 
-        ylab= "Emotional Valence",
-        col="red"
-)
+# p_obama2 <- ggplot(tidy_obama_tweets_polarity, aes(Sentiment,Total)) +
+#         geom_bar(stat="identity", position="dodge", fill="blue") +
+#         geom_bar(data=tidy_obama_tweets_polarity2, stat="identity", position="dodge", fill="red") +
+#         ylab("Total Weight") +
+#         ggtitle("Obama Tweet Sentiment (3/11/15)")
+# 
+# p_obama2
 
+p_obama2 <- ggplot(NULL, aes(Sentiment,Total)) +
+        geom_bar(aes(fill="march"), data=tidy_obama_tweets_polarity, stat="identity", alpha=0.5) +
+        geom_bar(aes(fill="april"), data=tidy_obama_tweets_polarity2, stat="identity", alpha=0.5) +
+        ylab("Total Weight") +
+        theme(legend.title=element_blank()) +
+        ggtitle("Obama Tweet Sentiment (March & April)")
+
+
+p_obama2
+
+# ## get sentiment with bing
+# 
+# obama_tweets_df$bing <- get_sentiment(obama_tweets_df$text, method="bing")
+# 
+# p_obama <-
+#         ggplot(obama_tweets_df, aes(created,bing)) +
+# #         geom_point(col="red", position="jitter") +
+#         geom_line(col="red") +
+#         geom_smooth(method=loess)
+# 
+# p_obama
+# 
+# ## get fourier transfor on bing sentiment
+# 
+# obama_sent_bing_ft <- get_transformed_values(obama_sent_bing)
+# 
+# p_obama_ft <-
+#         qplot(seq_along(obama_sent_bing_ft), obama_sent_bing_ft) +
+#         geom_line(col="red") +
+#         geom_point(col="red") +
+#         ylab("Valence") +
+#         xlab("Time (Percentage)") +
+#         ggtitle("#obama Tweets — Fourier Transform (3/11/15)")
+# 
+# p_obama_ft
 
 ## PART III — Sentiment Analysis (Survey Data)
 
@@ -167,8 +246,12 @@ survey_results <- read.csv("~/Sites/Shiny/survey_responses.csv")
 
 survey_results$Response.Text <- as.character(survey_results$Response.Text)
 
+## a little clean-up
+
 survey_results$Response.Date <- as.character(survey_results$Response.Date)
 survey_results$Response.Date <- as.Date(survey_results$Response.Date, format="%B %d %Y")
+
+## then something similar to what we did with obama tweets
 
 survey_nrc <- get_nrc_sentiment(survey_results$Response.Text)
 
@@ -179,14 +262,13 @@ library(dplyr)
 
 tidy_survey <- 
         survey_nrc %>%
-#         select(1:8) %>%
+        select(9:10) %>%
         gather() %>%
         group_by(Sentiment=key) %>%
         summarise(Total=sum(value))
 
-summary(tidy_survey)
-
-p_survey <- ggplot(tidy_survey, aes(Sentiment,Total)) +
+p_survey <- 
+        ggplot(tidy_survey, aes(Sentiment,Total)) +
         geom_bar(stat="identity") +
         ylab("Total Weight") +
         ggtitle("Survey Sentiment")
